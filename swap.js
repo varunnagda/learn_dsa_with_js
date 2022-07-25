672c215b5ff05689b4f6abb07d80a901cbf58300
Ajay Singh Rawat <rawatsingha@gmail.com>
2022-07-25 18:00:57 +0530

    swap array

---
+++ swap.js
@@ -0,0 +1,16 @@
+//swapping of two numbers without using third variable
+
+var firstNumber=10;
+var secondNumber=20;
+
+console.log("Before transpose the values are:-");
+console.log(`First Number=${firstNumber}`);
+console.log(`Seond Number=${secondNumber}`);
+
+firstNumber=firstNumber+secondNumber;
+secondNumber=firstNumber-secondNumber;
+firstNumber=firstNumber-secondNumber;
+
+console.log("Before transpose the values are:-");
+console.log(`First Number=${firstNumber}`);
+console.log(`Seond Number=${secondNumber}`);
